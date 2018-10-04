445d7b94cc5c9b7207967e9110cced4f393e2976
IO89 <osipov.ivan.89@gmail.com>
2018-10-04 15:24:34 +0300

    travis config

--- config/ci.js
+++ config/ci.js
@@ -1,7 +1,7 @@
 module.exports = {
     googleClientID: '719771379280-uet4p8vamtvj8jd4cd8relvdohn1q64s.apps.googleusercontent.com',
     googleClientSecret: 'Dv1UVZSiqetaEQRsyU2u_FTL',
-    mongoURI: 'mongodb://ivan:password1@ds151282.mlab.com:51282/blog_dev',
+    mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
     cookieKey:'quwhdqkwjdqwjdlkfadkjwjfqgwqw',
     redisUrl:'redis://127.0.0.1:6379'
 };
