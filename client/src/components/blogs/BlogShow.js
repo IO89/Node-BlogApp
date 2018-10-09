b83c56b78cb3b31d444848e69a623be1f6cc23b4
IO89 <osipov.ivan.89@gmail.com>
2018-10-09 15:13:00 +0300

    Images are uploaded to S3 Bucket

--- client/src/components/blogs/BlogShow.js
+++ client/src/components/blogs/BlogShow.js
@@ -1,40 +1,41 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { fetchBlog } from '../../actions';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {fetchBlog} from '../../actions';
 
 class BlogShow extends Component {
-  componentDidMount() {
-    this.props.fetchBlog(this.props.match.params._id);
-  }
+    componentDidMount() {
+        this.props.fetchBlog(this.props.match.params._id);
+    }
 
     renderImage() {
         if (this.props.blog.imageUrl) {
             return <img src={
                 'https://s3-eu-west-1.amazonaws.com/blog-app-675/' +
                 this.props.blog.imageUrl}
+                        alt={true}
             />
         }
     }
 
-  render() {
-    if (!this.props.blog) {
-      return '';
+    render() {
+        if (!this.props.blog) {
+            return '';
+        }
+
+        const {title, content} = this.props.blog;
+
+        return (
+            <div>
+                <h3>{title}</h3>
+                <p>{content}</p>
+                {this.renderImage()}
+            </div>
+        );
     }
-
-    const { title, content } = this.props.blog;
-
-    return (
-      <div>
-        <h3>{title}</h3>
-        <p>{content}</p>
-          {this.renderImage()}
-      </div>
-    );
-  }
 }
 
-function mapStateToProps({ blogs }, ownProps) {
-  return { blog: blogs[ownProps.match.params._id] };
+function mapStateToProps({blogs}, ownProps) {
+    return {blog: blogs[ownProps.match.params._id]};
 }
 
-export default connect(mapStateToProps, { fetchBlog })(BlogShow);
+export default connect(mapStateToProps, {fetchBlog})(BlogShow);
